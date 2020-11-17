878640c94e7e0dd8f10a221f9f9fe670ae7273c2
Mika Dumont <midumont@microsoft.com>
2020-11-16 16:19:06 -0800

    Add project files.

---
+++ BraceAnalyzer/BraceAnalyzer.CodeFixes/BraceAnalyzerCodeFixProvider.cs
@@ -0,0 +1,56 @@
+﻿namespace BraceAnalyzer
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Rename;
+    using Microsoft.CodeAnalysis.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(BraceAnalyzerCodeFixProvider)), Shared]
+    public class BraceAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(BraceAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title:"Add Braces",
+                    createChangedDocument: c => AddBracesAsync(root, context.Document, diagnosticSpan),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        Task<Document> AddBracesAsync(SyntaxNode root, Document document, TextSpan diagnosticSpan)
+        {
+            // Find the statement to wrap in braces.
+            var statement = root.FindNode(diagnosticSpan).FirstAncestorOrSelf<StatementSyntax>();
+            // Compute brace indication.
+            return Task.FromResult(document.WithSyntaxRoot(root.ReplaceNode(statement, SyntaxFactory.Block(statement))));
+        }
+    }
+}
---
+++ BraceAnalyzer/BraceAnalyzer/BraceAnalyzerAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿namespace BraceAnalyzer
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Threading;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class BraceAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "BraceAnalyzer";
+
+        private static readonly string Title = "Enclose with braces";
+        private static readonly string MessageFormat = @"Add ""{""";
+        private static readonly string Description = "Enclose with Braces";
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSyntaxTreeAction(syntaxTreeContext =>
+            {
+                // Iterate through all statements in the tree
+                var root = syntaxTreeContext.Tree.GetRoot(syntaxTreeContext.CancellationToken);
+                foreach (var statement in root.DescendantNodes().OfType<StatementSyntax>())
+                {
+                    // Skip analyzing block statements 
+                    if (statement is BlockSyntax)
+                    {
+                        continue;
+                    }
+                    // Report issues for all statements that are nested within a statement
+                    // but not a block statement
+                    if (statement.Parent is StatementSyntax && !(statement.Parent is BlockSyntax))
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, statement.GetFirstToken().GetLocation());
+                        syntaxTreeContext.ReportDiagnostic(diagnostic);
+                    }
+                }
+            });
+        }
+    }
+}
